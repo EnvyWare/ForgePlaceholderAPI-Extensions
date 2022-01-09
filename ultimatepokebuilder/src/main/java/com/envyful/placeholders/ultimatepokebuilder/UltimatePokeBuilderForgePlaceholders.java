8d5d37ce51b46baef9184dc53643d233617c578d
Daniel Norris <dnorris443@gmail.com>
2022-01-09 16:41:45 +0000

    feat(upb): update to new API

--- ultimatepokebuilder/src/main/java/com/envyful/placeholders/ultimatepokebuilder/UltimatePokeBuilderForgePlaceholders.java
+++ ultimatepokebuilder/src/main/java/com/envyful/placeholders/ultimatepokebuilder/UltimatePokeBuilderForgePlaceholders.java
@@ -1,47 +1,22 @@
 package com.envyful.placeholders.ultimatepokebuilder;
 
-import com.envyful.papi.api.PlaceholderManager;
+import com.envyful.papi.api.manager.AbstractPlaceholderManager;
+import com.envyful.placeholders.ultimatepokebuilder.extension.BalanceExtension;
 import com.envyful.ultimate.poke.builder.forge.UltimatePokeBuilderForge;
 import com.envyful.ultimate.poke.builder.forge.eco.handler.EcoFactory;
 import com.envyful.ultimate.poke.builder.forge.shade.envy.api.forge.player.ForgeEnvyPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 
-public class UltimatePokeBuilderForgePlaceholders implements PlaceholderManager<EntityPlayerMP> {
+public class UltimatePokeBuilderForgePlaceholders extends AbstractPlaceholderManager<EntityPlayerMP> {
 
-    @Override
-    public String getIdentifier() {
-        return "upb";
-    }
+    private static final String IDENTIFIER = "upb";
+    private static final String[] AUTHORS = new String[] { "Envyful" };
+    private static final String VERSION = "2.0.0";
+    private static final String NAME = "upb";
 
-    @Override
-    public String[] getAuthors() {
-        return new String[] { "Envyful" };
-    }
+    public UltimatePokeBuilderForgePlaceholders() {
+        super(IDENTIFIER, AUTHORS, VERSION, NAME);
 
-    @Override
-    public String getVersion() {
-        return "1.0.0";
-    }
-
-    @Override
-    public String getName() {
-        return getIdentifier();
-    }
-
-    @Override
-    public String onPlaceholderRequest(EntityPlayerMP player, String placeholder) {
-        String[] args = placeholder.split("_");
-
-        if (args.length < 2) {
-            return "UNDEFINED";
-        }
-
-        ForgeEnvyPlayer player1 = UltimatePokeBuilderForge.getInstance().getPlayerManager().getPlayer(player);
-
-        if (player1 == null) {
-            return "0";
-        }
-
-        return EcoFactory.getBalance(player1) + "";
+        this.registerPlaceholder(new BalanceExtension());
     }
 }
---
+++ ultimatepokebuilder/src/main/java/com/envyful/placeholders/ultimatepokebuilder/extension/BalanceExtension.java
@@ -0,0 +1,38 @@
+package com.envyful.placeholders.ultimatepokebuilder.extension;
+
+import com.envyful.papi.api.manager.extensions.AbstractExtension;
+import com.envyful.ultimate.poke.builder.forge.UltimatePokeBuilderForge;
+import com.envyful.ultimate.poke.builder.forge.eco.handler.EcoFactory;
+import com.envyful.ultimate.poke.builder.forge.shade.envy.api.forge.player.ForgeEnvyPlayer;
+import com.google.common.collect.Lists;
+import net.minecraft.entity.player.EntityPlayerMP;
+
+import java.util.List;
+
+public class BalanceExtension extends AbstractExtension<EntityPlayerMP> {
+
+    private static final String NAME = "balance";
+    private static final int PRIORITY = 1;
+    private static final List<String> DESCRIPTION = Lists.newArrayList("Gets the token balance");
+    private static final List<String> EXAMPLES = Lists.newArrayList("%upb_balance%");
+
+    public BalanceExtension() {
+        super(NAME, PRIORITY, DESCRIPTION, EXAMPLES);
+    }
+
+    @Override
+    public boolean matches(EntityPlayerMP player, String placeholder) {
+        return true;
+    }
+
+    @Override
+    public String parse(EntityPlayerMP player, String placeholder) {
+        ForgeEnvyPlayer player1 = UltimatePokeBuilderForge.getInstance().getPlayerManager().getPlayer(player);
+
+        if (player1 == null) {
+            return null;
+        }
+
+        return EcoFactory.getBalance(player1) + "";
+    }
+}
