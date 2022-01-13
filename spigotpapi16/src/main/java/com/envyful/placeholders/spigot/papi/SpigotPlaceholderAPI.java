d4378d01bf8cd85c1829d099130e95d5a0f8da5b
Daniel Norris <dnorris443@gmail.com>
2022-01-13 14:04:15 +0000

    feat(spigotpapi16): implement support

---
+++ spigotpapi16/src/main/java/com/envyful/placeholders/spigot/papi/SpigotPlaceholderAPI.java
@@ -0,0 +1,19 @@
+package com.envyful.placeholders.spigot.papi;
+
+import com.envyful.papi.api.manager.AbstractPlaceholderManager;
+import com.envyful.placeholders.spigot.papi.extension.SpigotPAPIExtension;
+import net.minecraft.entity.player.ServerPlayerEntity;
+
+public class SpigotPlaceholderAPI extends AbstractPlaceholderManager<ServerPlayerEntity> {
+
+    private static final String IDENTIFIER = "spigotpapi";
+    private static final String[] AUTHORS = new String[] { "Envyful" };
+    private static final String VERSION = "2.0.0";
+    private static final String NAME = "spigotpapi";
+
+    public SpigotPlaceholderAPI() {
+        super(IDENTIFIER, AUTHORS, VERSION, NAME);
+
+        this.registerPlaceholder(new SpigotPAPIExtension());
+    }
+}
