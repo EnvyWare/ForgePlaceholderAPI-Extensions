5bed6b0dccd695dff54cd1bbf3e23314f38ecf40
TheKnightKarim <44905678+TheKnightKarim@users.noreply.github.com>
2022-03-13 14:26:15 +0100

    Fixed misspelled example in PartyBallExtension (#9)
    Just spotted this when I was testing this extesion, small change so it doesn't confuse people when written to file using ADMIN_DEBUG command.

--- reforged/src/main/java/com/envyful/placeholders/reforged/extension/party/impl/PartyBallExtension.java
+++ reforged/src/main/java/com/envyful/placeholders/reforged/extension/party/impl/PartyBallExtension.java
@@ -12,7 +12,7 @@ public class PartyBallExtension extends PartyReforgedExtension {
     private static final String NAME = "ball";
     private static final int PRIORITY = 1;
     private static final List<String> DESCRIPTION = Lists.newArrayList("Gets the pokemon's ball in the given slot");
-    private static final List<String> EXAMPLES = Lists.newArrayList("%reforged_party_slot_slot_1_ball%");
+    private static final List<String> EXAMPLES = Lists.newArrayList("%reforged_party_slot_1_ball%");
 
     public PartyBallExtension() {
         super(NAME, PRIORITY, DESCRIPTION, EXAMPLES);
