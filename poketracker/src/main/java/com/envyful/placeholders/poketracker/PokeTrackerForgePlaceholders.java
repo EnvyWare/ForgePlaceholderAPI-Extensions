45dea04bd21a166d9223ab86a027cee5e3123481
Daniel Norris <dnorris443@gmail.com>
2022-06-20 16:26:02 +0100

    chore(gradle): bump version to 2.9.1

--- poketracker/src/main/java/com/envyful/placeholders/poketracker/PokeTrackerForgePlaceholders.java
+++ poketracker/src/main/java/com/envyful/placeholders/poketracker/PokeTrackerForgePlaceholders.java
@@ -18,7 +18,7 @@ public class PokeTrackerForgePlaceholders extends AbstractPlaceholderManager<Ent
 
     private static final String IDENTIFIER = "poketracker";
     private static final String[] AUTHORS = new String[] { "Envyful" };
-    private static final String VERSION = "2.9.0";
+    private static final String VERSION = "2.9.1";
     private static final String NAME = "poketracker";
 
     public static final DateFormat DATE_FORMATTER = new SimpleDateFormat("dd/MM/yyyy");
