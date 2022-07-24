5fde0f910b84127e09ed3145824e9bf21aa438fe
Daniel Norris <dnorris443@gmail.com>
2022-07-24 18:17:11 +0100

    chore(gradle): bump version to 2.9.2

--- ultimatepokebuilder/src/main/java/com/envyful/placeholders/ultimatepokebuilder/UltimatePokeBuilderForgePlaceholders.java
+++ ultimatepokebuilder/src/main/java/com/envyful/placeholders/ultimatepokebuilder/UltimatePokeBuilderForgePlaceholders.java
@@ -11,7 +11,7 @@ public class UltimatePokeBuilderForgePlaceholders extends AbstractPlaceholderMan
 
     private static final String IDENTIFIER = "upb";
     private static final String[] AUTHORS = new String[] { "Envyful" };
-    private static final String VERSION = "2.9.1";
+    private static final String VERSION = "2.9.2";
     private static final String NAME = "upb";
 
     public UltimatePokeBuilderForgePlaceholders() {
