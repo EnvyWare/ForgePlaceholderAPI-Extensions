a3c0d6e91d67db67c38916308134af9e3d085429
Daniel Norris <dnorris443@gmail.com>
2022-08-25 18:14:20 +0100

    feat(upb16): add UPB 1.16.5 placeholders

---
+++ ultimatepokebuilder16/src/main/java/com/envyful/placeholders/ultimatepokebuilder/UltimatePokeBuilderForgePlaceholders.java
@@ -0,0 +1,19 @@
+package com.envyful.placeholders.ultimatepokebuilder;
+
+import com.envyful.papi.api.manager.AbstractPlaceholderManager;
+import com.envyful.placeholders.ultimatepokebuilder.extension.BalanceExtension;
+import net.minecraft.entity.player.ServerPlayerEntity;
+
+public class UltimatePokeBuilderForgePlaceholders extends AbstractPlaceholderManager<ServerPlayerEntity> {
+
+    private static final String IDENTIFIER = "upb";
+    private static final String[] AUTHORS = new String[] { "Envyful" };
+    private static final String VERSION = "2.9.3";
+    private static final String NAME = "upb";
+
+    public UltimatePokeBuilderForgePlaceholders() {
+        super(IDENTIFIER, AUTHORS, VERSION, NAME, ServerPlayerEntity.class);
+
+        this.registerPlaceholder(new BalanceExtension());
+    }
+}
