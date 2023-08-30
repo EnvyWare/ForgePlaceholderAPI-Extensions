cfdfc22a207a4591be19de8c0e8d0239ed0dd086
Daniel Norris <dnorris443@gmail.com>
2023-08-30 13:26:13 +0100

    feat(spigotpapi20): add forge 1.20.1 version of the spigot papi extension

---
+++ spigotpapi20/src/main/java/com/envyful/placeholders/spigot/papi/SpigotPlaceholderAPI.java
@@ -0,0 +1,19 @@
+package com.envyful.placeholders.spigot.papi;
+
+import com.envyful.papi.api.manager.AbstractPlaceholderManager;
+import com.envyful.placeholders.spigot.papi.extension.SpigotPAPIExtension;
+import net.minecraft.server.level.ServerPlayer;
+
+public class SpigotPlaceholderAPI extends AbstractPlaceholderManager<ServerPlayer> {
+
+    private static final String IDENTIFIER = "spigotpapi";
+    private static final String[] AUTHORS = new String[] { "Envyful" };
+    private static final String VERSION = "3.0.1";
+    private static final String NAME = "spigotpapi";
+
+    public SpigotPlaceholderAPI() {
+        super(IDENTIFIER, AUTHORS, VERSION, NAME, ServerPlayer.class);
+
+        this.registerPlaceholder(new SpigotPAPIExtension());
+    }
+}
